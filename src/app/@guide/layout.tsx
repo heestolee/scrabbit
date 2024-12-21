a501995788ef3b19b353310a7b4dbad5c6c45d54
Heestolee <dlckdgml25@gmail.com>
2024-12-22 06:53:44 +0900

    Feat: Implement guide with parallel routing

---
+++ src/app/@guide/layout.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import ChakraLayout from "@/components/layout/ChakraLayout";
+
+export default function GuideLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ChakraLayout>
+      <Box
+        position="fixed"
+        top="20px"
+        right="20px"
+        zIndex="10"
+      >
+        {children}
+      </Box>
+    </ChakraLayout>
+  );
+}
---
+++ src/app/@guide/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { GuidePopover } from "@/components/guide/GuidePopover";
+
+export default function Guide() {
+  return (
+    <div>
+      <GuidePopover />
+    </div>
+  );
+}
